3db96374bdc3d0ca4a2dd900cc492bc422aac24b
Hasan <hasan@kaist.ac.kr>
2020-07-27 17:21:56 +0900

    nested catkin_ws deleted

--- catkin_ws/src/catkin_ws/src/cam_checker/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-# fetch values from package.xml
-
-setup(**generate_distutils_setup(
-    packages=['cam_checker'],
-    package_dir={'': 'src'}
-))
