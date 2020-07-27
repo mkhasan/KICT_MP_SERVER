9e4388d43728fbc6f3566d4d39944d0da327c9d5
Hasan <hasan@kaist.ac.kr>
2020-07-27 15:39:25 +0900

    erp42 added

---
+++ catkin_ws/build/erp42_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hasan/MyProjects/KICT_MP_SERVER/catkin_ws/src/erp42_control
+# Build directory: /home/hasan/programs/MyProjects/KICT_MP_SERVER/catkin_ws/build/erp42_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/src/catkin_ws/src/car_controller/src/car_controller_node.cpp
@@ -0,0 +1,13 @@
+/*
+ * car_contoller_node.cpp
+ *
+ *  Created on: Jul 23, 2020
+ *      Author: hasan
+ */
+
+
+
+
+int main() {
+	return 0;
+}
